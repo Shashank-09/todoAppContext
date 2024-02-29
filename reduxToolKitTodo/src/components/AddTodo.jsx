828c3d94a2728dce0910ef9548c035ade6fb976a
Shashank-09 <shashanktulsikar979@gmail.com>
2024-02-29 23:44:07 +0530

    reduxtoolkit done

--- reduxToolKitTodo/src/components/AddTodo.jsx
+++ reduxToolKitTodo/src/components/AddTodo.jsx
@@ -20,11 +20,11 @@ const AddTodo = () => {
        placeholder='Enter a todo'
        value={input}
        onChange={(e) => setInput(e.target.value)} 
-     >
+     />
        <button type='submit'
          className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'
-       /> Add Todo
-     </input>
+       > Add Todo
+     </button>
     </form>
   )
 }
